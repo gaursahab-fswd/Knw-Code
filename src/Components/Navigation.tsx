27c6c9feaf8dd48233b45f4594111c8cd6f72283
itprogaur <professionalcodehub@gmail.com>
2022-02-02 15:24:46 +0530

    Added Home Component

---
+++ src/Components/Navigation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () =>{
+    return(
+        <div className='text-center'>
+            <h1>HI NAVIGATION</h1>
+        </div>
+    );
+}
+
+export default Navigation;
