b9f1d91a2b83c2623a9e4676f92b18451ee6bfeb
itprogaur <professionalcodehub@gmail.com>
2022-02-02 22:57:50 +0530

    Collapse menu Added

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,51 +1,87 @@
-import React from 'react';
-import Logo from '../common/logo.png';
-import './Navigation.styles.css';
+import React from "react";
+import Logo from "../common/logo.png";
+import "./Navigation.styles.css";
 
-const Navigation = () =>{
-    return(
-        <div>
-            <nav className="navbar navbar-expand-lg  bg-dark text-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#"><img src={Logo} alt='Logo-image' /></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Interview Prep</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Online Courses
+const Navigation = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={Logo} alt="Logo-image" />
           </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a className="dropdown-item" href="#">Front-End Developement</a></li>
-            <li><a className="dropdown-item" href="#">Backend Development</a></li>
-            <li><a className="dropdown-item" href="#">Full-stack Web Development</a></li>
-            <li><a className="dropdown-item" href="#">Android Developement</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Events</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contact us</a>
-        </li>
-        <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-      </ul>
-    </div>
-  </div>
-</nav>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Interview Prep
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Online Courses
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Front-End Developement
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Full-stack Web Development
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Android Developement
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Events</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Contact us</a>
+              </li>
+            </ul>
+            <form className="d-flex justify-content-center">
+              <button className="btn btn-outline-default Nav-login" type="submit">
+                LogIn
+              </button> &nbsp;&nbsp;
+              <button className="btn btn-outline-default Nav-Enr" type="submit">
+                Enroll Now
+              </button>
+            </form>
+          </div>
         </div>
-    );
-}
+      </nav>
+    </div>
+  );
+};
 
 export default Navigation;
